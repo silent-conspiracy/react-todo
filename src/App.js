4a5e4faba45b881aaa267886fbfc3186d49b261e
Leon Yeoh <kwyeoh1@e.ntu.edu.sg>
2018-04-16 01:08:22 +0800

    Demonstrating React routers using react-router-dom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import TodoList from './TodoList';
 import TodoInput from './TodoInput';
+import { Switch, Route } from 'react-router-dom';
 
 class App extends Component {
   constructor() {
@@ -29,12 +30,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Todo App</h1>
         </header>
-        <TodoInput addFunction={this.add.bind(this)}>
-          <h2>Add New Todo</h2>
-        </TodoInput>
-        <TodoList listItems={this.state.todoList}>
-          <h2>Current Todo List</h2>
-        </TodoList>
+        <Switch>
+          <Route exact path='/' render={ () => (
+              <TodoList listItems={this.state.todoList}>
+                <h2>Current Todo List</h2>
+              </TodoList>
+          )} />
+
+          <Route path='/add' render={ () => (
+              <TodoInput addFunction={this.add.bind(this)}>
+                <h2>Add New Todo</h2>
+              </TodoInput>
+          )} />
+        </Switch>
       </div>
     );
   }
