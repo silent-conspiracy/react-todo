2e6c0428cf1e19c9cd46d144c8df44c32ba771fd
Leon Yeoh <kwyeoh1@e.ntu.edu.sg>
2018-04-15 11:23:55 +0800

    Demonstrating conditional rendering

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,18 +1,42 @@
 import React from 'react';
 
 class TodoInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ""
+    }
+  }
+
+  handleChange() {
+    let todo = this.refs._todoInput.value;
+    this.setState({
+      inputValue: todo
+    })
+  }
 
   addTodoList() {
-    let todo = this.refs._todoInput.value;
-    this.props.addFunction(todo);
+    this.props.addFunction(this.state.inputValue);
+    this.setState({
+      inputValue: ""
+    });
+    this.refs._todoInput.value = "";
+  }
+
+  renderButton() {;
+    if (this.state.inputValue) {
+      return (<button onClick={this.addTodoList.bind(this)}>Add</button>);
+    } else {
+      return (<button disabled="true">Add</button>);
+    }
   }
 
   render() {
     return (
       <div>
         {this.props.children}
-        <input ref="_todoInput" type="text" />
-        <button onClick={this.addTodoList.bind(this)}>Add</button>
+        <input ref="_todoInput" type="text" onChange={this.handleChange.bind(this)} />
+        {this.renderButton()}
       </div>
     )
   }
